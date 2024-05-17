6f4c61a1d6aa75d7224d34be21ca3d19659eaa83
Abraham John <jabrahamjohns@gmail.com>
2024-05-17 14:15:56 +0300

    this boiler plate is awesome to go

--- utils/redis.js
+++ utils/redis.js
@@ -1,6 +1,16 @@
+import {promisify} from 'util';
+import { createClient } from 'redis';
+
+
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
+    this.isClientConnected = true;
+    this.client.on('error', (err) => {
+      console.log('Redis client failed to connect:', err.message || err.toString());
+      this.isClientConnected = false;
+    }
+    
   }
 
   set(key, value) {
